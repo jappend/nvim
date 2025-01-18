de48a8ddd3fd8341c6ac9c8415f2bcf945e75d87
João <taboadajoao19122235@gmail.com>
2025-01-18 22:27:36 +0000

    added lazy.nvim package manager and catppucin theme

--- init.lua
+++ init.lua
@@ -1,3 +1,27 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+}
+local opts = {}
+
+require("lazy").setup(plugins, opts)
+
 -- Basic Config
 vim.opt.number = true			-- Show line count
 vim.opt.relativenumber = true		-- Relative numbers (good for navigation)
@@ -16,7 +40,7 @@ vim.opt.clipboard = "unnamedplus"	-- Use system's default clipboard
 -- Interface Config
 vim.opt.termguicolors = true		-- Turn on terminal colors
 vim.opt.signcolumn = "yes"		-- Always show sign column
-vim.cmd("colorscheme desert")		-- Choose basic theme
+vim.cmd("colorscheme catppuccin \" catppucin-macchiato \" ")		-- Choose basic theme
 
 -- Nice keymapping
 vim.g.mapleader = " "			-- Configure "space" as leader key
