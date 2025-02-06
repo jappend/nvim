937b4b8956178e7a78f531401f4eb17f4218d79c
João <taboadajoao19122235@gmail.com>
2025-02-06 22:26:05 +0000

    feat: added java lsp to my config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "gopls" }
+        ensure_installed = { "lua_ls", "gopls", "jdtls" }
       })
     end
   },
@@ -34,7 +34,12 @@ return {
             },
             staticcheck = true, -- Habilitar staticcheck
           }
-        }
+        },
+      })
+      lspconfig.jdtls.setup({
+        capabilities = capabilities,
+        cmd = { "jdtls" },
+        root_dir = lspconfig.util.root_pattern("pom.xml", "build.gradle", ".git")
       })
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = {"lua", "javascript", "go"},
+      ensure_installed = {"lua", "javascript", "go", "java"},
 	    highlight = { enable = true },
 	    indent = { enable = true },
     })
